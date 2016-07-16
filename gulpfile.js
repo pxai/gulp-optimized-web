876e67289fbad018057cf0e0a989dbaec14fd6fe
pelloxabier <pello.altadill@gmail.com>
2016-07-16 18:29:49 +0200

    Added custom js, modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,24 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+
+gulp.task('say_hello', function() {
+	console.log('Gulp says hello');
+});
+
+gulp.task('just_move', function () {
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+	server : {
+	 baseDir: 'dist'
+	}
+  });
+});
 
 // Lint JS
 gulp.task('lint', function() {
@@ -22,10 +40,12 @@ gulp.task('minify', function(){
 });
 
 // Watch Our Files
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync','minify'],function() {
   //gulp.watch('src/*.js', ['lint', 'minify']);
-  gulp-watch('src/js/*.js', ['minify']);
+  gulp.watch('src/**/*', ['minify']);
 });
 
 // Default
+gulp.task('move',['just_move']);
 gulp.task('default', ['minify', 'watch']);
+gulp.task('greet',['say_hello']);
---
+++ src/js/custom.js
@@ -0,0 +1,8 @@
+/**
+* custom.js
+* Custom JavaScript files
+*/
+var Greeter = function () {
+console.log('ok, greeting ready');
+};
+
